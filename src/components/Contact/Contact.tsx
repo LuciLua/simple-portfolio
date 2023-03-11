8a5b717aa9be84e2654afc6ca9efcc395993b261
LuciLua <luci.lua81@gmail.com>
2023-03-11 19:31:46 -0300

    [features] 🦝 motion and tilt interactive animation on cards projects

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -5,21 +5,23 @@ import styles from "./Contact.module.scss"
 function Contact() {
     return (
         <div className={styles.container}>
-            <h1>📧 Email Me</h1>
-            <form>
-                <label htmlFor="name">Name</label>
-                <input id="name" type="text" />
-                <label htmlFor="email">Email</label>
-                <input id="email" type="text" />
-                <label htmlFor="title">Title</label>
-                <input id="title" type="text" />
-                <label htmlFor="msg">Message</label>
-                <textarea id="msg" />
-                <div className={styles.btn_collection}>
-                    <button onClick={() => alert('not works yet')} type="submit">Send</button>
-                    <button type="reset">Reset</button>
-                </div>
-            </form>
+            <div className={styles.content}>
+                <h1>📧 Email Me</h1>
+                <form>
+                    <label htmlFor="name">Name</label>
+                    <input id="name" type="text" />
+                    <label htmlFor="email">Email</label>
+                    <input id="email" type="text" />
+                    <label htmlFor="title">Title</label>
+                    <input id="title" type="text" />
+                    <label htmlFor="msg">Message</label>
+                    <textarea id="msg" />
+                    <div className={styles.btn_collection}>
+                        <button onClick={() => alert('not works yet')} type="submit">Send</button>
+                        <button type="reset">Reset</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,39 +1,38 @@
+'use client'
+
 import Image from "next/image"
 import styles from "./Projects.module.scss"
 
+import Tilt from "react-tilt"
+import { motion } from "framer-motion";
+// import { fadeIn, textVariant } from "../../utils/motion";
+
 function Projects() {
+
+    const ServiceCard = ({ title, imgPath, imgAlt, href }) => (
+        <Tilt className={styles.project} >
+            <motion.h1 whileHover={{ x: -2, backgroundColor:"rgb(210, 210, 210, 0.9)" }}>
+                <motion.a href={href}>
+                    {title}
+                </motion.a>
+            </motion.h1>
+            <div className={styles.c_preview}>
+                <Image sizes="100" src={imgPath} fill alt={imgAlt} />
+            </div>
+        </Tilt>
+    );
+
     return (
         <div className={styles.container}>
             <h1>My projects</h1>
             <div className={styles.projects_grid}>
                 <div className={styles.row_one}>
-                    <div className={styles.project}>
-                        <h1>Dognos</h1>
-                        <div className={styles.c_preview}>
-                            <Image sizes="100" src={'/dognos_preview.png'} fill alt="Dognos"/>
-                        </div>
-                    </div>
-                    <div className={styles.project}>
-                        <h1>Todo List</h1>                        <div className={styles.c_preview}>
-                            <Image sizes="100" src={'/todo_preview.png'} fill alt="ToDo" />
-                        </div>
-                    </div>
+                    <ServiceCard key={'dognos-project'} title="Dognos" imgPath="/dognos_preview.png" imgAlt="dognos-project" href="https://dognos.vercel.app/" />
+                    <ServiceCard key={'todo-list-project'} title="Todo List" imgPath="/todo_preview.png" imgAlt="todo-list-project" href="https://todo-lucilua.vercel.app/" />
                 </div>
                 <div className={styles.row_two}>
-                    <div className={styles.project}>
-                        <h1>Overtherminal</h1>
-                        <div className={styles.c_preview}>
-                            <Image sizes="100" src={'/overtherminal_preview.png'} fill alt="Overtherminal" />
-                        </div>
-                    </div>
-                    <div className={styles.project}>
-                        <h1>
-                            CLI: Python requests
-                        </h1>
-                        <div className={styles.c_preview}>
-                            <Image sizes="100" src={'/pythonCLI_preview.png'} fill alt="Overtherminal" />
-                        </div>
-                    </div>
+                    <ServiceCard key={'overtherminal_project'} title="Overtherminal" imgPath="/overtherminal_preview.png" imgAlt="overtherminal-project" href="https://github.com/LuciLua/consoleElectron" />
+                    <ServiceCard key={'CLI-python-requests-project'} title="CLI: Python requests" imgPath="/pythonCLI_preview.png" imgAlt="CLI-python-requests-project" href="https://github.com/LuciLua/python_studies_1/tree/master/requestsCli" />
                 </div>
             </div>
         </div >
