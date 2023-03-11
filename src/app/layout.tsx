92e7b414bff4ce495fa371b99ddd51b996387944
LuciLua <luci.lua81@gmail.com>
2023-03-11 20:51:50 -0300

    [PWA] add manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ export default function RootApp({ children }) {
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>Simples Portfolio</title>
+                <link rel="manifest" href="manifest.json" />
             </head>
             <body className={`${ubuntu.className} light`}>
                 <BtnToggleTheme />
